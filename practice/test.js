8776c16bc68c7acc58946e77b2376a5bc5148618
Ayush ghosh <ayushghosh637@gmail.com>
2026-05-04 20:12:58 +0530

    Enhance test.js with string manipulation examples
    Added variables and additional console logs for name, surname, and string manipulation examples.

--- practice/test.js
+++ practice/test.js
@@ -1,1 +1,15 @@
 console.log("ayush ghosh")
+
+ const name = "ayush"
+    const sirname = "ghosh"
+
+    //console.log(name + sirname)
+   console.log(`my first name is ${name} and my last name is ${sirname}`)
+   const h= "harrypotter"
+   console.log(h.substring(0,4))
+   //similarly we have slice for the same task but slice as supports negative intergers which can allow reverse indexing
+
+   const n = "   brown fox      "
+   // trim function removes unwanted spces
+   console.log(n.trim())
+   console.log(n)
